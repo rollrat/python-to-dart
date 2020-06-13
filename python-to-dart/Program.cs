8fa953b4f0ef6de10784e4e81f47e300479e3233
violet-dev <violet.dev.master@gmail.com>
2020-06-13 13:40:37 +0900

    test: Parser test

--- python-to-dart/Program.cs
+++ python-to-dart/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Antlr4.Runtime;
+using python_to_dart.Parser;
+using System;
 
 namespace python_to_dart
 {
@@ -6,7 +8,33 @@ namespace python_to_dart
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var code = @"""
+# with_stmt: ASYNC? WITH with_item (COMMA with_item)* COLON suite
+
+# WITH with_item COLON suite
+with open(""with_stmt.py""):
+    pass
+
+# WITH with_item COMMA with_item COLON suite
+with open(""with_stmt.py"") as a, open(""with_stmt.py"") as b:
+    pass
+
+# async_stmt must be inside async function
+async def f():
+    # ASYNC with_stmt
+    async with open(""with_stmt.py"") as f:
+        pass
+""";
+
+            ICharStream target = new AntlrInputStream(code);
+            ITokenSource lexer = new PythonLexer(target);
+            ITokenStream tokens = new CommonTokenStream(lexer);
+            PythonParser parser = new PythonParser(tokens);
+            parser.BuildParseTree = true;
+
+            var result = parser.root();
+
+
         }
     }
 }
