6cca56a1227032154e8cb953d7dbfae5b2ca6dab
violet-dev <violet.dev.master@gmail.com>
2020-06-13 13:12:11 +0900

    init: init base code

---
+++ python-to-dart/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace python_to_dart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
